6d8b61c75bfffded91c908ca5076cebea1e60512
volontyr <alex.volontyr@gmail.com>
2016-11-21 01:31:15 +0200

    Handling messages in queue depending on the message type

--- app/classes/MessageHandlers/messages_queue_handler.rb
+++ app/classes/MessageHandlers/messages_queue_handler.rb
@@ -1,5 +1,8 @@
 require_relative 'message_sender'
 require_relative '../../../app/classes/constants'
+require_relative 'message_initializer'
+require_relative 'message_generator'
+require_relative 'received_message_handler'
 
 class MessagesQueueHandler
 
@@ -8,6 +11,7 @@ class MessagesQueueHandler
     @node = node
     @network = network
     @message_sender = MessageSender.new(network)
+    @message_receiver = ReceivedMessageHandler.new(network)
   end
 
 
@@ -17,7 +21,10 @@ class MessagesQueueHandler
       next if buffer.empty?
       message = next_message(buffer)
 
-      unless message.receiver_node == @node.id
+      if message.receiver_node == @node.id
+        # handle_received_message
+        @message_receiver.receive_message(@node, message)
+      else
         next_node = next_receiver_node(message.receiver_node)
         next_channel = @network.find_channel(@node.id, next_node)
 
@@ -25,7 +32,7 @@ class MessagesQueueHandler
         if next_channel.has_message?(message)
           @message_sender.send_message(@node.id, next_node, message)
         else
-          add_message_to_out_buffer(message, next_channel)
+          add_message_to_out_buffer(message, next_channel, buffer)
         end
       end
     end
@@ -33,7 +40,17 @@ class MessagesQueueHandler
 
 
   def next_message(buffer)
-    buffer.delete_at(0)
+    if @network.message_sending_mode == :logical_connection
+      if @network.established_connection?(buffer[0].sender_node, buffer[0].receiver_node)
+        return buffer[0]
+      else
+        return MessageGenerator.new(@network).create_manage_message(
+                  :request, buffer[0].sender_node, buffer[0].receiver_node
+               )
+      end
+    else
+      buffer[0]
+    end
   end
 
 
@@ -42,10 +59,29 @@ class MessagesQueueHandler
   end
 
 
-  def add_message_to_out_buffer(message, channel)
+  def add_message_to_out_buffer(message, channel, previous_buffer=nil)
     unless (buffer = channel.get_buffer_by_node(@node.id)).nil?
       message.delivery_time += Constants.node_ping_time
-      buffer << message
+      if [:info, :update_routes_tables].include?(message.type)
+        buffer << message
+      else
+        buffer.insert(0, message)
+      end
+
+      unless previous_buffer.nil? or message.type != :request
+        previous_buffer.delete(message)
+        if channel.is_busy
+          MessageGenerator.new(@network).create_manage_message(
+              :negative_response, @node.id, message.sender_node
+          )
+          buffer.delete(message)
+        else
+          receiver = @node.routes_table[message.sender_node.to_s][0]
+          MessageGenerator.new(@network).create_manage_message(
+              :positive_response, @node.id, receiver
+          )
+        end
+      end
     end
   end
 
